6d8a794dcef11e2de7894f6fea5e53cf51594acf
enthusiast94 <manas.bajaj94@gmail.com>
2020-03-22 12:05:02 +0000

    Delted common config since it wasnt used

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,31 +1,42 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const webpackCommon = require('./webpack.common');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-webpackCommon.entry = {
-    main: './src/index.tsx'
-};
-
-webpackCommon.output = {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-};
-
-webpackCommon.devServer = {
-    contentBase: './dist'
-};
-
-webpackCommon.plugins = [
-    ...webpackCommon.plugins, 
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.tsx"
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
     new HtmlWebpackPlugin({
-        title: 'Webpack Playground',
-        template: './src/index.html'
+      title: "Webpack Playground",
+      template: "./src/index.html"
     }),
     new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        openAnalyzer: false
+      analyzerMode: "static",
+      openAnalyzer: false
     })
-];
-
-module.exports = webpackCommon;
+  ]
+};
